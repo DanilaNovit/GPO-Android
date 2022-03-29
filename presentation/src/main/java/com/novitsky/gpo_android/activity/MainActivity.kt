78566071d43e869bd024d8ad4fe6425da372db03
Danila Novitsky <dnovitsky@tomsksoft.com>
2022-03-29 22:03:09 +0700

    Base implementation MainActivity

--- presentation/src/main/java/com/novitsky/gpo_android/activity/MainActivity.kt
+++ presentation/src/main/java/com/novitsky/gpo_android/activity/MainActivity.kt
@@ -3,14 +3,27 @@ package com.novitsky.gpo_android.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.novitsky.gpo_android.R
+import com.novitsky.gpo_android.navigation.RootRouter
 
 class MainActivity : AppCompatActivity(), ClosingAppActivity {
+	private val router = RootRouter(applicationContext, supportFragmentManager, R.id.main_container, this)
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		showUI()
+	}
+
+	override fun onBackPressed() {
+		router.back()
 	}
 
 	override fun close() {
 		finish()
+	}
+
+//	#TODO: Impl
+	private fun showUI() {
+
 	}
 }
