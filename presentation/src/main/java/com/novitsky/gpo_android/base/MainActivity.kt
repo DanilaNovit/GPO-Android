f3c372d968aa9de4580ccc759b213beeb1eb94ff
Danila Novitsky <dnovitsky@tomsksoft.com>
2022-03-29 21:39:55 +0700

    Prepare presentation for fragments

--- presentation/src/main/java/com/novitsky/gpo_android/base/MainActivity.kt
+++ presentation/src/main/java/com/novitsky/gpo_android/base/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.novitsky.gpo_android
+package com.novitsky.gpo_android.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.novitsky.gpo_android.R
+//	#TODO: imp
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
